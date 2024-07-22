2aca282ed5fb11b789dbb0af57914f57d79fdd18
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-22 13:01:10 +0530

    db-demo-java

--- src/com/db/demo/col/ColDemo.java
+++ src/com/db/demo/col/ColDemo.java
@@ -1,29 +1,36 @@
 package com.db.demo.col;
 
+import java.util.ArrayList;
+
 // Collection Framework 
 
 public class ColDemo {
 
 	public static void main(String[] args) {
 
-		String[] myFrields = { "Sonu", "Monu", "Tonu" };
+		ArrayList myFriends = new ArrayList();
+		System.out.println(myFriends);
+		System.out.println(myFriends.size());
+		myFriends.add("Sonu");
+		myFriends.add("Monu");
+		myFriends.add("Tonu");
+		System.out.println(myFriends);
+		System.out.println(myFriends.size());
+		myFriends.remove(1);
+		System.out.println(myFriends);
+		System.out.println(myFriends.size());
 
-		System.out.println("Extract array elements using for loop:");
-		for (int i = 0; i < myFrields.length; i++) {
-			System.out.println(myFrields[i]);
-		}
+//		String[] myFriends2 = { "Sonu", "Monu", "Tonu" }; // "Ponu"
+//		myFriends2.
 
-		System.out.println("Extract array elements using for-each loop:");
-		for (String frind : myFrields) {
-			System.out.println(frind);
-		}
+//		System.out.println("Extract array elements using for loop:");
+//		System.out.println("Extract array elements using for-each loop:");
+
 	}
 
 }
 
 //package com.db.demo.col;
-//
-//import com.db.demo.model.Employee;
 //
 //// Collection Framework 
 //
@@ -31,13 +38,38 @@ public class ColDemo {
 //
 //	public static void main(String[] args) {
 //
-//		int num = 10;
-//		long phone = 9876543210L;
-////		phone.
+//		String[] myFrields = { "Sonu", "Monu", "Tonu" }; // "Ponu"
 //
-//		Employee employee = new Employee();
-////		employee.
+//		System.out.println("Extract array elements using for loop:");
+//		for (int i = 0; i < myFrields.length; i++) {
+//			System.out.println(myFrields[i]);
+//		}
 //
+//		System.out.println("Extract array elements using for-each loop:");
+//		for (String frind : myFrields) {
+//			System.out.println(frind);
+//		}
 //	}
 //
 //}
+//
+////package com.db.demo.col;
+////
+////import com.db.demo.model.Employee;
+////
+////// Collection Framework 
+////
+////public class ColDemo {
+////
+////	public static void main(String[] args) {
+////
+////		int num = 10;
+////		long phone = 9876543210L;
+//////		phone.
+////
+////		Employee employee = new Employee();
+//////		employee.
+////
+////	}
+////
+////}
