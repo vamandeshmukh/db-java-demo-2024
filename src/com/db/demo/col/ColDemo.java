d4664c0b8146e3552a317a7cc6e7f1bc0209caca
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-22 12:41:24 +0530

    db-demo-java

--- src/com/db/demo/col/ColDemo.java
+++ src/com/db/demo/col/ColDemo.java
@@ -6,6 +6,38 @@ public class ColDemo {
 
 	public static void main(String[] args) {
 
+		String[] myFrields = { "Sonu", "Monu", "Tonu" };
+
+		System.out.println("Extract array elements using for loop:");
+		for (int i = 0; i < myFrields.length; i++) {
+			System.out.println(myFrields[i]);
+		}
+
+		System.out.println("Extract array elements using for-each loop:");
+		for (String frind : myFrields) {
+			System.out.println(frind);
+		}
 	}
 
 }
+
+//package com.db.demo.col;
+//
+//import com.db.demo.model.Employee;
+//
+//// Collection Framework 
+//
+//public class ColDemo {
+//
+//	public static void main(String[] args) {
+//
+//		int num = 10;
+//		long phone = 9876543210L;
+////		phone.
+//
+//		Employee employee = new Employee();
+////		employee.
+//
+//	}
+//
+//}
---
+++ src/com/db/demo/col/WrapperDemo.java
@@ -0,0 +1,18 @@
+package com.db.demo.col;
+
+public class WrapperDemo {
+
+	public static void main(String[] args) {
+		
+//		int num = 10;
+//	
+////		num.
+//		
+//		Integer num2 = 20;
+//		
+//		num2.
+		
+
+	}
+
+}
