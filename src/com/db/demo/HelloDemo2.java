64c9f599d2060a57168e809b13760bacb4c7a389
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-22 12:09:08 +0530

    db-demo-java

--- src/com/db/demo/HelloDemo2.java
+++ src/com/db/demo/HelloDemo2.java
@@ -2,4 +2,14 @@ package com.db.demo;
 
 public class HelloDemo2 {
 
+	public static void main(String[] args) {
+
+		System.out.println("Hi there!");
+
+		int num = 10 + 2;
+
+		System.out.println(num);
+
+	}
+
 }
