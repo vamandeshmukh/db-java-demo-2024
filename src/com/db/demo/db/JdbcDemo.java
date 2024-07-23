71c73eb7ed1ac883b72b2317554ba93abdccf9ca
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-23 11:15:58 +0530

    day-1 remaining topics

---
+++ src/com/db/demo/db/JdbcDemo.java
@@ -0,0 +1,78 @@
+package com.db.demo.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcDemo {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		String url = "jdbc:mysql://localhost:3306/sakila";
+		String user = "root";
+		String password = "root";
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+		conn = DriverManager.getConnection(url, user, password);
+		System.out.println("Connected to the database successfully!");
+
+		stmt = conn.createStatement();
+
+		String sql = "SELECT * FROM sakila.actor";
+		rs = stmt.executeQuery(sql);
+
+		while (rs.next()) {
+			String firstName = rs.getString("first_name");
+			System.out.println("First Name: " + firstName);
+		}
+		rs.close();
+		stmt.close();
+		conn.close();
+	}
+}
+
+
+//package com.db.demo.db;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//
+//public class JdbcDemo {
+//	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//
+//		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+//		String user = "root";
+//		String password = "root";
+//
+//		Connection conn = null;
+//		Statement stmt = null;
+//		ResultSet rs = null;
+//
+//		Class.forName("oracle.jdbc.driver.OracleDriver");
+//
+//		conn = DriverManager.getConnection(url, user, password);
+//		System.out.println("Connected to the database successfully!");
+//
+//		stmt = conn.createStatement();
+//
+//		String sql = "SELECT * FROM sakila.actor";
+//		rs = stmt.executeQuery(sql);
+//
+//		while (rs.next()) {
+//			String firstName = rs.getString("first_name");
+//			System.out.println("First Name: " + firstName);
+//		}
+//		rs.close();
+//		stmt.close();
+//		conn.close();
+//	}
+//}
