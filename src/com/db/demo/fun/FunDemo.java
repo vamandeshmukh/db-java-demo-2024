4b13f638a35e40ec093a6a1d4827ff1e708f5eec
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-23 10:14:09 +0530

    day-1 remaining topics

--- src/com/db/demo/fun/FunDemo.java
+++ src/com/db/demo/fun/FunDemo.java
@@ -1,29 +1,56 @@
 package com.db.demo.fun;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class FunDemo {
 
 	public static void main(String[] args) {
 
-//		ArrayList<String> myList = new ArrayList<>();
 		List<String> myList = new ArrayList<>();
-//		myList = new LinkedList<>();
 
 		myList.addAll(Arrays.asList("Sonu", "Monu", "Tonu", "Soni", "Ponu"));
 
-//		System.out.println("Extract list elements with for loop:");
-//		for (int i = 0; i < myList.size(); i++)
-//			System.out.println(myList.get(i));
+//		System.out.println("Extract list elements with forEach method:");
+//		myList.forEach(elem ->  System.out.println(elem) );
 
-		System.out.println("Extract list elements with forEach method:");
-//		myList.forEach(() -> {});
-//		myList.forEach((elem) -> { System.out.println(elem); });
-		myList.forEach(elem ->  System.out.println(elem) );
-
+//		System.out.println("Print names starting with 'S' with for and if:");
+//		for (int i = 0; i < myList.size(); i++) {
+//			if (myList.get(i).startsWith("S")) {
+//				System.out.println(myList.get(i));
+//			}
+//		}
+		System.out.println("Print names starting with 'S' with stream API:");
+		myList.stream().filter( name -> name.startsWith("S")).forEach(name -> System.out.println(name));
 	}
 
 }
+
+//package com.db.demo.fun;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.LinkedList;
+//import java.util.List;
+//
+//public class FunDemo {
+//
+//	public static void main(String[] args) {
+//
+////		ArrayList<String> myList = new ArrayList<>();
+//		List<String> myList = new ArrayList<>();
+////		myList = new LinkedList<>();
+//
+//		myList.addAll(Arrays.asList("Sonu", "Monu", "Tonu", "Soni", "Ponu"));
+//
+////		System.out.println("Extract list elements with for loop:");
+////		for (int i = 0; i < myList.size(); i++)
+////			System.out.println(myList.get(i));
+//
+//		System.out.println("Extract list elements with forEach method:");
+////		myList.forEach(() -> {});
+////		myList.forEach((elem) -> { System.out.println(elem); });
+//		myList.forEach(elem ->  System.out.println(elem) );
+//
+//	}
+//
+//}
