bc04284c353adbd7b9d0719047f350c4ed029286
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-23 11:52:45 +0530

    day-1 remaining topics

--- src/com/db/demo/db/JdbcDemo.java
+++ src/com/db/demo/db/JdbcDemo.java
@@ -1,17 +1,16 @@
+//Code for VM - 
+
 package com.db.demo.db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class JdbcDemo {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "root";
-		String password = "root";
+		String url = "jdbc:oracle:thin:@localhost:1521:ORCL";
+		String user = "System";
+		String password = "oracle";
+		String sql = "SELECT * FROM employees";
 
 		Connection conn = null;
 		Statement stmt = null;
@@ -20,16 +19,16 @@ public class JdbcDemo {
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 
 		conn = DriverManager.getConnection(url, user, password);
-		System.out.println("Connected to the database successfully!");
+		System.out.println(conn.toString());
 
 		stmt = conn.createStatement();
+		System.out.println(stmt.toString());
 
-		String sql = "SELECT * FROM sakila.actor";
 		rs = stmt.executeQuery(sql);
+		System.out.println(rs.toString());
 
 		while (rs.next()) {
-			String firstName = rs.getString("first_name");
-			System.out.println("First Name: " + firstName);
+			System.out.println(rs.getString("first_name"));
 		}
 		rs.close();
 		stmt.close();
@@ -37,7 +36,45 @@ public class JdbcDemo {
 	}
 }
 
-
+//package com.db.demo.db;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//
+//public class JdbcDemo {
+//	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//
+//		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+//		String user = "root";
+//		String password = "root";
+//
+//		Connection conn = null;
+//		Statement stmt = null;
+//		ResultSet rs = null;
+//
+//		Class.forName("oracle.jdbc.driver.OracleDriver");
+//
+//		conn = DriverManager.getConnection(url, user, password);
+//		System.out.println("Connected to the database successfully!");
+//
+//		stmt = conn.createStatement();
+//
+//		String sql = "SELECT * FROM sakila.actor";
+//		rs = stmt.executeQuery(sql);
+//
+//		while (rs.next()) {
+//			String firstName = rs.getString("first_name");
+//			System.out.println("First Name: " + firstName);
+//		}
+//		rs.close();
+//		stmt.close();
+//		conn.close();
+//	}
+//}
+//
 
 //package com.db.demo.db;
 //
@@ -77,5 +114,3 @@ public class JdbcDemo {
 //		conn.close();
 //	}
 //}
-
-
