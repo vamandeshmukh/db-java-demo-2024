2b0f987c919c65d4a85d30a458cbd98f31f3436a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-23 12:35:57 +0530

    day-1 remaining topics

--- src/com/db/demo/db/JdbcDemo.java
+++ src/com/db/demo/db/JdbcDemo.java
@@ -1,3 +1,44 @@
+//-- slightly better formatted code 
+
+package com.db.demo.jdbc;
+
+import java.sql.*;
+
+public class JdbcDemo {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		String url = "jdbc:oracle:thin:@localhost:1521:ORCL";
+		String user = "System";
+		String password = "oracle";
+		String sql = "SELECT * FROM SYSTEM.EMPLOYEES";
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		ResultSetMetaData rsmd = null;
+
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		conn = DriverManager.getConnection(url, user, password);
+		stmt = conn.createStatement();
+		rs = stmt.executeQuery(sql);
+		rsmd = rs.getMetaData();
+
+		System.out.println("-- Employees Data --");
+
+		for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+			System.out.print(rsmd.getColumnName(i) + " ");
+		}
+		System.out.println();
+		while (rs.next()) {
+			System.out.println(rs.getInt("eid") + " " + rs.getString("first_name") + " " + rs.getDouble("salary"));
+		}
+		rs.close();
+		stmt.close();
+		conn.close();
+	}
+}
+
 ////Code for VM - 
 //
 //package com.db.demo.db;
@@ -91,40 +132,40 @@
 //}
 //
 
-package com.db.demo.db;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-public class JdbcDemo {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-
-		String url = "jdbc:mysql://localhost:3306/sakila";
-		String user = "root";
-		String password = "root";
-
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-
-		Class.forName("com.mysql.cj.jdbc.Driver");
-
-		conn = DriverManager.getConnection(url, user, password);
-		System.out.println("Connected to the database successfully!");
-
-		stmt = conn.createStatement();
-
-		String sql = "SELECT * FROM sakila.actor ORDER BY first_name";
-		rs = stmt.executeQuery(sql);
-
-		while (rs.next()) {
-			System.out.println(rs.getString("first_name"));
-		}
-		rs.close();
-		stmt.close();
-		conn.close();
-	}
-}
+//package com.db.demo.db;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//
+//public class JdbcDemo {
+//	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//
+//		String url = "jdbc:mysql://localhost:3306/sakila";
+//		String user = "root";
+//		String password = "root";
+//
+//		Connection conn = null;
+//		Statement stmt = null;
+//		ResultSet rs = null;
+//
+//		Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//		conn = DriverManager.getConnection(url, user, password);
+//		System.out.println("Connected to the database successfully!");
+//
+//		stmt = conn.createStatement();
+//
+//		String sql = "SELECT * FROM sakila.actor ORDER BY first_name";
+//		rs = stmt.executeQuery(sql);
+//
+//		while (rs.next()) {
+//			System.out.println(rs.getString("first_name"));
+//		}
+//		rs.close();
+//		stmt.close();
+//		conn.close();
+//	}
+//}
